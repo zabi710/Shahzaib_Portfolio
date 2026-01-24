1c9c387c2ef859281460e82ecb3ce547111c7ed2
Mian Shahzaib <mianshahzaiboutlook@gmail.com>
2026-01-24 18:16:21 +0500

    Initial commit from Create Next App

---
+++ app/Home/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-amber-600 min-h-screen '>
+    <h1 className='text-3xl text-rose-500 font-bold'>Hello Shahzaib</h1>
+      
+    </div>
+  )
+}
+
+export default page
